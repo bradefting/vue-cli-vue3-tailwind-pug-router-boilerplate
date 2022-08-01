e78343c680745c1c2520814ea255f8985572c316
BRADEFTING <BRADEFTING@GMAIL.COM>
2022-08-01 14:47:03 -0400

    replace node-sass with sass

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -59,12 +59,13 @@ h3 {
 ul {
   list-style-type: none;
   padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+
+  li {
+    display: inline-block;
+    margin: 0 10px;
+    a {
+      color: #42b983;
+    }
+  }
 }
 </style>
